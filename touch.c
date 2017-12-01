e4c367cf369ca0d5786aeed7e2c99fd44e59e0da
andikaandra <andikaandra03@gmail.com>
2017-12-02 01:39:44 +0700

    add rm and renew touch

--- touch.c
+++ touch.c
@@ -8,13 +8,13 @@ int main(int argc, char *argv[])
 	int i,fd1;
 
 	if(argc < 2){
-		printf(2, "Usage: cant create\n");
+		printf(2, "need at least 2 arguments\n");
 		exit();
 	}
 
 	for(i = 1; i < argc; i++){
 		if((fd1 = open(argv[i], O_CREATE|O_RDWR)) < 0){
-			printf(2, "touch: %s failed to create\n", argv[i]);
+			printf(2, "failed to create %s\n", argv[i]);
 			break;
 		}
 	}
