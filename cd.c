828fe7bd4350ed267fedc6b93511c414cceed27f
andikaandra <andikaandra03@gmail.com>
2017-12-05 12:17:47 +0700

    salah upload

--- cd.c
+++ cd.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "types.h"
 #include "stat.h"
 #include "user.h"
@@ -15,28 +14,3 @@ int main(int argc, char *argv[]){
   }
   exit();
 }
-=======
-#include "types.h"
-#include "stat.h"
-#include "user.h"
-
-int
-main(int argc, char *argv[])
-{
-  int i;
-
-  if(argc < 2){
-    printf(2, "Usage: chdir files...\n");
-    exit();
-  }
-
-  for(i = 1; i < argc; i++){
-    if(chdir(argv[i]) < 0){
-      printf(2, "chdir: %s failed to create\n", argv[i]);
-      break;
-    }
-  }
-
-  exit();
-}
->>>>>>> 417b2c74f2c875045517083de0ac42d56b9b7d6e
